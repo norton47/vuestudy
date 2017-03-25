6287c93082b0b75011a07236727089d9384d020a
rebitprogr4 <rebit.progr4@gmail.com>
2017-03-26 00:49:59 +0200

    2. Биндинг + тригеры

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'HW',
-        link: 'http://google.com',
-        finishedLink: '<a href="http://google.com"> Google </a>'
+        counter: 0,
+        x: 0,
+        y: 0,
+        name: 'Oleg',
+
+        counterNew: 0
     },
     methods: {
-        sayHello: function () {
-            this.title = 'Hello!';
-            return this.title
+        increase: function (cou) {
+
+            return this.counter += cou  ;
+        },
+        updateCoordinates: function (event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
+        dummy: function (event) {
+            //Останавливает работу ивента
+            event.stopPropagation();
+        },
+        alertMe: function (event) {
+            alert(event.value);
+        }
+
+    },
+    // Тригер для модели
+    watch: {
+        counterNew: function(value) {
+            var vm = this;
+            setTimeout(function () {
+                vm.counterNew = 0;
+            }, 2000)
+        }
+    },
+    // Триггер как переменная, которую можно разместить в шаблоне
+    computed: {
+        output: function (){
+            return this.counterNew > 5? "More then 5" : "Smoler then 5";
         }
     }
 });
