be07d8dc3f8a8f28a5824dbf4650f08b7d135a56
rebitprogr4 <rebit.progr4@gmail.com>
2017-03-25 22:39:57 +0200

    2. Варианты связывания

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-new Vue ({
-   el: '#app',
+new Vue({
+    el: '#app',
     data: {
-       title: 'HW'
+        title: 'HW',
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com"> Google </a>'
     },
     methods: {
-        changeTitle: function (event) {
-            this.title = event.target.value
+        sayHello: function () {
+            this.title = 'Hello!';
+            return this.title
         }
     }
 });
