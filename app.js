b03284ece693bc8393bdbcfc36afd804b7d7e909
rebitprogr4 <rebit.progr4@gmail.com>
2017-03-26 16:09:37 +0300

    3. Работа с if

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 new Vue({
     el: '#app',
     data: {
-        attachRed: false,
-        color: 'green',
-        width: 100
-    },
-    computed: {
-        divClasses: function () {
-            return {
-                red: this.attachRed,
-                blue: !this.attachRed
-            }
-        },
-        myStyle: function () {
-            return {
-                backgroundColor: this.color,
-                width: this.width + 'px'
-            }
-        }
+        show: true
     }
 });
